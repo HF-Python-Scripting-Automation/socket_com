5f6c877d92e3b715f7c165fadefe2aa7832e8a2e
vmadmin <vmadmin@kali-linux.smartlearn.lan>
2026-03-31 03:05:27 +0200

    [change] logger_config: wrap both loggers in get function to avoid execution by import.

--- socket_com/utils/logger_conifg.py
+++ socket_com/utils/logger_conifg.py
@@ -1,7 +1,8 @@
 import logging
-from psa_utils.logger import get_logger, configure_logging
+from psa_utils.logger import get_logger
 
+def get_server_logger():
+    return get_logger("Server", filename="server.log", level=logging.DEBUG, clear=True)
 
-configure_logging(log_dir="logs", clear_old_logs=True)
-server_logger = get_logger("Server", filename="server.log", level=logging.DEBUG)
-client_logger = get_logger("Client", filename="client.log", level=logging.DEBUG)
+def get_client_logger():
+    return get_logger("Client", filename="client.log", level=logging.DEBUG, clear=True)
